7f78f5e9df65435c473afa86fc1c013c74293f24
Brett Mandler <brettmandler@gmail.com>
2023-01-17 11:08:34 -0500

    Added character counter

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import {ChangeEventHandler, FC, HTMLInputTypeAttribute} from 'react'
 
 interface InputFieldProps {
@@ -29,8 +30,22 @@ export const InputField: FC<InputFieldProps> = ({
       onChange={onChange}
       value={value}
       id={id}
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+      className={classNames(
+        'bg-gray-50 border  text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
+        value.length > 200
+          ? 'text-red-400 border-red-500 bg-red-400/0.2 focus:border-red-500'
+          : 'text-gray-900 border-gray-300'
+      )}
       placeholder={placeholder}
     ></input>
+    <p
+      id="helper-text-explanation"
+      className={classNames(
+        'mt-2 text-lg ',
+        value.length > 200 ? 'text-red-400' : 'text-gray-500'
+      )}
+    >
+      ({value.length}/200) characters used
+    </p>
   </div>
 )
